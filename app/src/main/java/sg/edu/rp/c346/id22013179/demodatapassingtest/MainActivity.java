a6397bf6a9154a252556e1febe055c1dfcfa6761
sokhiennn <22013179@myrp.edu.sg>
2023-05-17 22:32:50 +0800

    3rd activity halfway done

--- app/src/main/java/sg/edu/rp/c346/id22013179/demodatapassingtest/MainActivity.java
+++ app/src/main/java/sg/edu/rp/c346/id22013179/demodatapassingtest/MainActivity.java
@@ -10,6 +10,7 @@ import android.widget.Button;
 public class MainActivity extends AppCompatActivity {
 
     Button btnPassInteger;
+    Button btnPassChar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,12 +18,21 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         btnPassInteger = findViewById(R.id.buttonPassInteger);
-
+        btnPassChar = findViewById(R.id.buttonPassChar);
         btnPassInteger.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(MainActivity.this, SecondActivity.class);
                 intent.putExtra("value", 1);
+                startActivity(intent);
+            }
+        });
+
+        btnPassChar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, ThirdActivity.class);
+                intent.putExtra("value","a");
                 startActivity(intent);
             }
         });
